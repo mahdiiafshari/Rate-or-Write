1ada85ee7fdd07f54cb8d753cd3b57a2d5a72880
mahdiiafshari <mahdiafshari661384@gmail.com>
2025-07-18 11:44:06 +0330

    drf extended.

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',  # Custom user app
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
