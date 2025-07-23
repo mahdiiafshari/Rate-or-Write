52fcc402e81aa41f2fc078bc986b13b3e0463add
mahdiiafshari <mahdiafshari661384@gmail.com>
2025-07-23 16:18:24 +0330

    competition serializer extended

---
+++ Backend/competition/serializer.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Competitor ,Competition
+
+class CompetitionSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = Competition
+        fields = ['id', 'name','status' , 'is_public' , 'bio' , 'category']
+        read_only_fields = ['created_at', 'updated_at']
+
+    def get_competitor_count(self, obj):
+        return obj.competitor_count()
+
+
+
+class CompetitorSerializer(serializers.ModelSerializer):
+    competition = CompetitionSerializer(read_only=True)
+    competition_id = serializers.PrimaryKeyRelatedField(
+        queryset=Competition.objects.all(), source='competition', write_only=True
+    )
+
+    class Meta:
+        model = Competitor
+        fields = ['id', 'competition', 'competition_id', 'user', 'post', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
+
