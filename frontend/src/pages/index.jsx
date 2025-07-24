5c667a051b06b0050f391699a7c632dbeb91c4b5
mahdiiafshari <mahdiafshari661384@gmail.com>
2025-07-24 14:43:28 +0330

    FIX:update ui css

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,17 +1,20 @@
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 export default function Home() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>Home Page</h1>
-      <button onClick={() => navigate("/login")}>
-        Go to Login
-      </button>
-        <button onClick={() => navigate("/create-post")}>
-        Creat post
-       </button>
-    </div>
-  );
+    return (
+        <div style={{padding: "2rem", textAlign: "center"}}>
+            <h1>Home Page</h1>
+            <button className='download-button' onClick={() => navigate("/login")}>
+                Go to Login
+            </button>
+            <button className='download-button' onClick={() => navigate("/create-post")}>
+                Creat post
+            </button>
+            <button className='download-button' onClick={() => navigate("/posts")}>
+                See posts
+            </button>
+        </div>
+    );
 }
